233fccec4c3473790b6a296ab6d463b16b9b6dcb
joakin <joaquin@chimeces.com>
2022-12-13 18:19:03 +0100

    Add changes-per-year flag for a date based report

--- main.ts
+++ main.ts
@@ -1,5 +1,23 @@
 #!/usr/bin/env -S deno run --allow-net --allow-write=./data --allow-read=./data
 
+interface GerritChange {
+  created: string;
+  insertions: number;
+  deletions: number;
+  _more_changes?: true;
+}
+
+interface ChangesStatsByString {
+  [key: string]: ChangesStats;
+}
+
+interface ChangesStats {
+  Changes: number;
+  Insertions: number;
+  Deletions: number;
+  "Ins + Del LoC": number;
+}
+
 if (import.meta.main) {
   const cmd = Deno.args[0];
   switch (cmd) {
@@ -12,55 +30,85 @@ if (import.meta.main) {
     case "changes-per-project": {
       const user = Deno.args[1];
       if (!user) throw new Error("No user passed in as an argument");
-      const decoder = new TextDecoder("utf-8");
-      let data;
-      try {
-        data = await Deno.readFile(changesPath(user));
-      } catch (_) {
-        await fetchAndWriteChanges(user);
-        data = await Deno.readFile(changesPath(user));
-      }
-      const text = decoder.decode(data);
-      const json = JSON.parse(text);
+      const changes = await fetchCachedChanges(user);
 
-      // console.log(json[0]);
-      const projects: {
-        [key: string]: {
-          changes: number;
-          insertions: number;
-          deletions: number;
-          total: number;
-        };
-      } = {};
-      const total = {
-        changes: 0,
-        insertions: 0,
-        deletions: 0,
-        total: 0,
-      };
+      const projects: ChangesStatsByString = {};
 
-      for (const change of json) {
+      for (const change of changes) {
         if (!projects[change.project]) {
           projects[change.project] = {
-            changes: 0,
-            insertions: 0,
-            deletions: 0,
-            total: 0,
+            Changes: 0,
+            Insertions: 0,
+            Deletions: 0,
+            "Ins + Del LoC": 0,
           };
         }
         const project = projects[change.project];
-        project.changes += 1;
-        total.changes += 1;
-        project.insertions += change.insertions;
-        total.insertions += change.insertions;
-        project.deletions += change.deletions;
-        total.deletions += change.deletions;
-        project.total += change.insertions + change.deletions;
-        total.total += change.insertions + change.deletions;
+        project.Changes += 1;
+        project.Insertions += change.insertions;
+        project.Deletions += change.deletions;
+        project["Ins + Del LoC"] += change.insertions + change.deletions;
       }
-      projects["total"] = total;
+      addTotal(projects);
 
       console.table(projects);
+      break;
+    }
+    case "changes-per-year": {
+      const user = Deno.args[1];
+      if (!user) throw new Error("No user passed in as an argument");
+      const changes: GerritChange[] = await fetchCachedChanges(user);
+      changes.sort((a, b) =>
+        a.created > b.created ? 1 : a.created === b.created ? 0 : -1
+      );
+
+      const yearMonths: ChangesStatsByString = {};
+
+      for (const change of changes) {
+        const creationDate = new Date(change.created + "Z");
+        const year = String(creationDate.getUTCFullYear());
+        const month = String(creationDate.getUTCMonth() + 1).padStart(2, "0");
+        const creationYearMonth = `${year}-${month}`;
+
+        if (!yearMonths[creationYearMonth]) {
+          yearMonths[creationYearMonth] = {
+            Changes: 0,
+            Insertions: 0,
+            Deletions: 0,
+            "Ins + Del LoC": 0,
+          };
+        }
+        const project = yearMonths[creationYearMonth];
+        project.Changes += 1;
+        project.Insertions += change.insertions;
+        project.Deletions += change.deletions;
+        project["Ins + Del LoC"] += change.insertions + change.deletions;
+      }
+
+      const years: ChangesStatsByString = {};
+      for (const change of changes) {
+        const creationDate = new Date(change.created + "Z");
+        const year = String(creationDate.getUTCFullYear());
+
+        if (!years[year]) {
+          years[year] = {
+            Changes: 0,
+            Insertions: 0,
+            Deletions: 0,
+            "Ins + Del LoC": 0,
+          };
+        }
+        const project = years[year];
+        project.Changes += 1;
+        project.Insertions += change.insertions;
+        project.Deletions += change.deletions;
+        project["Ins + Del LoC"] += change.insertions + change.deletions;
+      }
+
+      addTotal(years);
+
+      console.table(yearMonths);
+      console.table(years);
       break;
     }
     default: {
@@ -68,17 +116,33 @@ if (import.meta.main) {
 USAGE:
     fetch-changes [USERNAME|EMAIL]
     changes-per-project [USERNAME|EMAIL]
+    changes-per-year [USERNAME|EMAIL]
 `);
     }
   }
 }
 
-async function fetchAndWriteChanges(user: string) {
-  const res = await fetch(
-    `https://gerrit.wikimedia.org/r/changes/?q=owner:${user}`
-  );
+async function fetchChanges(
+  user: string,
+  start: number | null = null,
+  changes: GerritChange[] = []
+) {
+  const startParam = start ? `&start=${start}` : "";
+  const url = `https://gerrit.wikimedia.org/r/changes/?q=owner:${user}${startParam}`;
+  console.log(`Fetching ${url}`);
+  const res = await fetch(url);
   const text = await res.text();
-  const json = JSON.parse(text.slice(5));
+  const json: GerritChange[] = JSON.parse(text.slice(5));
+  changes.push(...json);
+
+  if (changes.length > 0 && changes[changes.length - 1]._more_changes) {
+    await fetchChanges(user, (start ?? 0) + json.length, changes);
+  }
+  return changes;
+}
+
+async function fetchAndWriteChanges(user: string) {
+  const json = await fetchChanges(user);
   console.log(`Found ${json.length} entries`);
   const encoder = new TextEncoder();
   const data = encoder.encode(JSON.stringify(json, null, 2));
@@ -89,4 +153,33 @@ async function fetchAndWriteChanges(user: string) {
 
 function changesPath(user: string) {
   return `./data/changes-${user}.json`;
+}
+
+async function fetchCachedChanges(user: string) {
+  const decoder = new TextDecoder("utf-8");
+  let data;
+  try {
+    data = await Deno.readFile(changesPath(user));
+  } catch (_) {
+    await fetchAndWriteChanges(user);
+    data = await Deno.readFile(changesPath(user));
+  }
+  const text = decoder.decode(data);
+  return JSON.parse(text);
+}
+
+function addTotal(stats: ChangesStatsByString) {
+  const total: ChangesStats = {
+    Changes: 0,
+    Insertions: 0,
+    Deletions: 0,
+    "Ins + Del LoC": 0,
+  };
+  for (const row of Object.values(stats)) {
+    total.Changes += row.Changes;
+    total.Insertions += row.Insertions;
+    total.Deletions += row.Deletions;
+    total["Ins + Del LoC"] += row.Insertions + row.Deletions;
+  }
+  stats["total"] = total;
 }
